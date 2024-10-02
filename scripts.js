a60a1eccd56327cfb84421d5e708598ea738ab0c
martin <martin.bartaa@gmail.com>
2024-10-02 17:16:38 +0300

    troubleshoot 4,5

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 async function fetchMigData() {
     const posMigration = await fetch("https://statfin.stat.fi/PxWeb/sq/4bb2c735-1dc3-4c5e-bde7-2165df85e65f").then(res => res.json());
     const negMigration = await fetch("https://statfin.stat.fi/PxWeb/sq/944493ca-ea4d-4fd9-a75c-4975192f7b6e").then(res => res.json());
-    return { posMigration, negMigration };
+    return {posMigration, negMigration};
 }
 
 function getColor(pos, neg) {
@@ -26,7 +26,7 @@ fetch("https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFe
         const geojsonLayer = L.geoJSON(geojsonData, {
             style: function(feature) {
                 const id = feature.id
-                const municipalityIndex = parseInt(id.split(".")[1])
+                const municipalityIndex = parseInt(id.split(".")[1] -1)
                 const posValue = positiveData.dataset.value[municipalityIndex];
                 const negValue = negativeData.dataset.value[municipalityIndex];
                 const color = getColor(posValue, negValue);
@@ -38,15 +38,11 @@ fetch("https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFe
             onEachFeature: function(feature, layer) {
                 layer.bindTooltip(feature.properties.name);
                 const id = feature.id;
-                const municipalityIndexPopup = parseInt(id.split(".")[1]);
+                const municipalityIndexPopup = parseInt(id.split(".")[1] -1);
                 const posValuePopup = positiveData.dataset.value[municipalityIndexPopup];
                 const negValuePopup = negativeData.dataset.value[municipalityIndexPopup];
                 layer.on("click", function () {
-                    const popupContent = `
-                        Municipality: ${feature.properties.name}<br>
-                        Positive migration: ${posValuePopup}<br>
-                        Negative migration: ${negValuePopup}
-                    `;
+                    const popupContent = feature.properties.name + posValuePopup + negValuePopup;
                     layer.bindPopup(popupContent).openPopup();
                 });
             }
